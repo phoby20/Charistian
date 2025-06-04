f172c9d190a41e72efd8a4165aa066d8d6021444
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-06-04 15:33:19 +0900

    뷰박스 에러 수정

--- src/components/UserDetailEditForm.tsx
+++ src/components/UserDetailEditForm.tsx
@@ -292,7 +292,7 @@ export default function UserDetailEditForm({
               className="w-6 h-6"
               fill="none"
               stroke="currentColor"
-              viewBox="0 24 24"
+              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path

4870bd2be4fedff37fce273f69b59e6af76789a4
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-06-05 09:53:55 +0900

    카드의 디자인 조정

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -81,7 +81,7 @@ export default function MembersPage() {
         {members.length === 0 ? (
           <p className="text-gray-500 italic">{t("noMembers")}</p>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 gap-3">
             {members.map((user) => (
               <MemberCard key={user.id} user={user} onClick={setSelectedUser} />
             ))}
--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useTranslation } from "react-i18next";
 import Image from "next/image";
 import { User } from "@/types/customUser";
@@ -14,7 +12,7 @@ export default function MemberCard({ user, onClick }: MemberCardProps) {
 
   return (
     <div
-      className="bg-white rounded-xl shadow-lg p-3 hover:shadow-xl transition-shadow duration-300 cursor-pointer flex items-center space-x-4"
+      className="bg-white rounded-xl shadow-lg p-2 hover:shadow-xl hover:border transition-shadow duration-300 cursor-pointer flex items-center space-x-4"
       onClick={() => onClick(user)}
       role="button"
       aria-label={t("viewUserDetails", { name: user.name })}
@@ -24,14 +22,28 @@ export default function MemberCard({ user, onClick }: MemberCardProps) {
         alt={user.name}
         width={60}
         height={50}
-        className="w-16 h-16 rounded-full object-cover border-2 border-gray-300"
+        className="w-14 h-14 rounded-full object-cover border-2 border-gray-300"
         onError={(e) => (e.currentTarget.src = "/default_user.png")}
       />
-      <div>
-        <p className="text-lg font-bold text-gray-900">{user.name}</p>
-        <p className="text-sm text-gray-500">
-          {user.position ? user.position.name : t("noPosition")}
-        </p>
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center  gap-3">
+          <p className="text-lg font-bold text-gray-900">{user.name}</p>
+          <p className="text-sm text-gray-500">
+            {user.position ? user.position.name : t("noPosition")}
+          </p>
+        </div>
+        <div className="flex gap-2">
+          {user.duties.map((duty) => {
+            return (
+              <p
+                className="text-xs text-gray-600 border border-green-500 bg-green-100 rounded-full px-2"
+                key={duty.id}
+              >
+                {duty.name}
+              </p>
+            );
+          })}
+        </div>
       </div>
     </div>
   );

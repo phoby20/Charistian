7e5312df4a3037b1f88f0be0f95c23f67538a5e8
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-06-17 15:27:00 +0900

    Header의 좌우 여백을 조정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -106,7 +106,7 @@ export default function Header() {
 
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-12">
           {/* 로고 및 사이트 제목 */}
           <div className="flex-shrink-0 flex items-center">

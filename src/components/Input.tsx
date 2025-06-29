0991e25523702e86b44e88e586ec11016ec38c25
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-06-29 17:22:23 +0900

    로그인 할때 입력부가 비활성화 되도록 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,22 @@
+// src/components/Input.tsx
 import { FC, InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-const Input: FC<InputProps> = ({ label, ...props }) => (
+const Input: FC<InputProps> = ({ label, disabled, ...props }) => (
   <div className="mb-4">
     <label className="block text-sm font-medium text-gray-500 mb-1">
       {label}
     </label>
-    <input className="mt-1 p-2 border rounded-md w-full" {...props} />
+    <input
+      className={`mt-1 p-2 border rounded-md w-full ${
+        disabled ? "bg-gray-200 cursor-not-allowed" : ""
+      }`}
+      disabled={disabled}
+      {...props}
+    />
   </div>
 );
 

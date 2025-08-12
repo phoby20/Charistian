c7c3a594cbcf46cf331cca27afd079fbd3bd8e94
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-08-12 15:57:05 +0900

    Stripe의 apiVersion 변경

--- src/app/api/secure/subscriptions/route.ts
+++ src/app/api/secure/subscriptions/route.ts
@@ -8,7 +8,7 @@ import { cookies } from "next/headers";
 import { checkErrors } from "./checkErrors";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-06-30.basil",
+  apiVersion: "2025-07-30.basil",
 });
 
 interface ResponseData {

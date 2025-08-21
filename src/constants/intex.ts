9a61d90c41e0dce99111f68f452d4f1dc80fcb33
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-08-21 18:56:42 +0900

    악보 리스트에서 박자 필터가 되도록 수정

--- src/constants/intex.ts
+++ src/constants/intex.ts
@@ -26,16 +26,10 @@ export const constants = {
     "2/4", // March or polka
     "3/2", // Renaissance or baroque music
     "3/4", // Waltz or minuet
-    "3/8", // Slow compound triple
     "4/2", // Early music
     "4/4", // Common time
-    "5/4", // Modern or jazz
-    "5/8", // Asymmetric meter
     "6/4", // Early music or compound duple
     "6/8", // Compound duple (e.g., jigs)
-    "7/4", // Progressive rock or modern
-    "7/8", // Balkan or progressive music
-    "9/8", // Compound triple
     "12/8", // Compound quadruple
   ],
 } as const;
